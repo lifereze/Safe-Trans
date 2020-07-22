f975d02f490d62d273eeced90847b53bdc86af2b
lifereze <liferezearnold@gmail.com>
2020-07-22 11:12:39 +0300

    add a abstract user class

--- src/main/java/model/tenant.java
+++ src/main/java/model/tenant.java
@@ -1,4 +1,25 @@
 package model;
 
-public class tenant {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class tenant extends user {
+
+    public static final String DATABASE_TYPE = "animal";
+
+    public tenant(String name) {
+        this.name = name;
+        this.type = DATABASE_TYPE;
+    }
+    public static List<tenant> all(){
+        try (Connection con = DB.sql2o.open()){
+            String queryAnimals ="SELECT * FROM animals WHERE type='animal'";
+            return con.createQuery(queryAnimals)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+
+        }
+    }
 }
+
--- src/main/java/model/user.java
+++ src/main/java/model/user.java
@@ -1,4 +1,62 @@
 package model;
+import org.sql2o.Connection;
+import java.util.Objects;
 
-public class user {
+public abstract class user<String> {
+
+    public int id;
+    public String name;
+    public int user_id;
+    public String location;
+    public String apartment;
+    public String type;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUserId() {
+        return user_id;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getApartment(){
+        return apartment;
+    }
+    public String getType(){
+        return type;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(type, user.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
+
+    public void save(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO users(name, type) VALUES(:name,:type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
